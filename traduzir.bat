f2972a7268f029dc12acb89ff491d6a9226c512c
cfarl <lutitus@gmail.com>
2024-09-06 13:43:09 -0300

    Traducao dos arquivos EV

--- traduzir.bat
+++ traduzir.bat
@@ -1,2 +1,2 @@
-java -cp .\tool lastremnant.unreal3.pc.TraduzArquivosVersaoPC "C:\Games\The Last Remnant" "C:\Users\P_6984\Desktop\traducao_TLR_PC\textos-traduzidos"
+java -cp .\tool lastremnant.unreal3.pc.TraduzArquivosVersaoPC "C:\Games\The Last Remnant" "C:\Users\cfarl\Desktop\traducao_TLR_PC\textos-traduzidos"
 pause
