1de11bb27388dc7c3f3ee3ada63f5c37224da7d0
cfarl <lutitus@gmail.com>
2024-09-14 02:18:21 -0300

    Adicionados arquivos de imagem.

--- traduzir.bat
+++ traduzir.bat
@@ -1,2 +1,2 @@
-java -cp .\tool lastremnant.unreal3.pc.TraduzArquivosVersaoPC "C:\Games\The Last Remnant" ".\textos-traduzidos"
+java -cp .\tool lastremnant.unreal3.pc.TraduzArquivosVersaoPC "C:\Games\The Last Remnant" 
 pause
